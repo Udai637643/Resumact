9a9e2a7e5e924a861d12bddbac64746b59809795
Udai <udai637643@gmail.com>
2025-03-29 17:02:07 +0530

    resume

---
+++ src/Components/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./About.css"
+
+function About() {
+  return (
+    <>
+      <div className='about-head'>About</div>
+      <div className='about'>
+      I am Udai Lal Regar, a passionate and results-driven Software Developer with expertise in React Native, MERN Stack and C++ programming. Currently pursuing B.Tech in Computer Science and Engineering at IIIT Una, I have gained hands-on experience in web and mobile app development, real-time communication, machine learning integrations, and scalable system design.
+        
+      </div>
+    </>
+  )
+}
+
+export default About
